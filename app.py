0ac5c7792820aeb6b84f8c425f06c66117033391
Виктория Филатова <filatova.v.m.0606@mail.ru>
2025-09-11 16:32:46 +0300

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def start ():
-    return """<!doctype html>" 
-        "<html>" 
-        "   <body>" 
-        "  <h1>web-сервер на flask</h1>" 
-        "   <body>" 
-        "</html>"""
+@app.route("/web")
+def web ():
+    return """<!doctype html>
+        <html>
+           <body>
+                <h1>web-сервер на flask</h1> 
+                <p><a href="/author">Информация об авторе</a></p>
+           </body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Филатова Виктория Михайловна"
+    group = "ФБИ-34"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
