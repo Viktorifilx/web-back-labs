33611e5cff6bb36947d0e7139a940aa0e61c7a28
Виктория Филатова <filatova.v.m.0606@mail.ru>
2025-09-12 09:16:26 +0300

    Стили

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def web ():
            </body>
         </html>""", 200, {
             "X-Server": "sample",
-            'Content-Type': 'text/html; charest=utf-8'
+            'Content-Type': 'text/html; charset=utf-8'
         }
 
 @app.route("/author")
@@ -39,15 +39,20 @@ def author():
 @app.route("/image")
 def image():
     path = url_for("static", filename="oak.jpg")
+    css_path = url_for("static", filename = "lab1.css")
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="''' + css_path + '''">
+    </head>
     <body>
         <h1>Дуб</h1>
-        <img src="'''+ path + '''">
+        <img src="''' + path + '''">
     </body>
 </html>
 '''
+
 count=0 
 
 @app.route('/counter')
