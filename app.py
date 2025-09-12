63adf14e3a0aacd8f67f4382afb3963988e6b213
Виктория Филатова <filatova.v.m.0606@mail.ru>
2025-09-12 11:39:59 +0300

    Меню первой лабораторной

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def index():
     return """<!doctype html>
 <html>
     <head>
-        <meta charset="utf-8">
         <title>НГТУ, ФБ, Лабораторные работы</title>
     </head>
     <body>
@@ -32,6 +31,22 @@ def index():
         <footer>
             <p>""" + name + """, """ + group + """, """ + course + """, """ + year + """</p>
         </footer>
+    </body>
+</html>"""
+
+@app.route("/lab1")
+def lab1():
+    return """<!doctype html>
+<html>
+    <head>
+        <title>Лабораторная 1</title>
+    </head>
+    <body>
+        <p>Flask — фреймворк для создания веб-приложений на языке программирования Python, 
+        использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2. 
+        Относится к категории так называемых микрофреймворков — минималистичных 
+        каркасов веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</p>
+        <p><a href="/">На главную</a></p>
     </body>
 </html>"""
 
