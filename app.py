881c1758c556f7d16ac47fed595a5948c8df5aad
Виктория Филатова <filatova.v.m.0606@mail.ru>
2025-10-29 16:58:49 +0300

    Заготовка 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 import datetime
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.errorhandler(404)
 def not_found(err):
@@ -35,6 +37,7 @@ def index():
                 <li><a href="/lab1">Первая лабораторная</a></li>
                 <li><a href="/lab2/">Вторая лабораторная</a></li>
                 <li><a href="/lab3/">Третья лабораторная</a></li>
+                <li><a href="/lab4/">Четвертая лабораторная</a></li>
             </ul>
         </nav>
         <footer>
